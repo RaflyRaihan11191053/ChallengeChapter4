5455c38fa4c03531a572aeefed7d88a7c2d3a014
raflyraihan <11191053@student.itk.ac.id>
2022-04-05 10:34:06 +0800

    Finish make sharedPreferences on Spalsh Screen and Welcome Screen

--- app/src/main/java/com/example/challengechapter4/spalshFragment.kt
+++ app/src/main/java/com/example/challengechapter4/spalshFragment.kt
@@ -3,17 +3,20 @@ package com.example.challengechapter4
 import android.content.Context
 import android.content.SharedPreferences
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.findNavController
+import androidx.core.os.HandlerCompat.postDelayed
 import androidx.navigation.fragment.findNavController
-import com.example.challengechapter4.databinding.FragmentWelcomeBinding
+import com.example.challengechapter4.databinding.FragmentSpalshBinding
 
-class welcomeFragment : Fragment() {
+class spalshFragment : Fragment() {
 
-    lateinit var binding: FragmentWelcomeBinding
+    lateinit var binding: FragmentSpalshBinding
 
     val sharedPreferences = "sharedPreferences"
 
@@ -21,26 +24,26 @@ class welcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentWelcomeBinding.inflate(layoutInflater)
+        binding = FragmentSpalshBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnWelcome.setOnClickListener {
+        Handler(Looper.getMainLooper()).postDelayed({
 
-            val welcomeScreen: SharedPreferences = requireActivity().getSharedPreferences(sharedPreferences, Context.MODE_PRIVATE)
-            val isFirstTime: Boolean = welcomeScreen.getBoolean("firstTime", true)
+            val splashScreen: SharedPreferences = requireActivity().getSharedPreferences(sharedPreferences, Context.MODE_PRIVATE)
+            val isFirstTime: Boolean = splashScreen.getBoolean("firstTime", true)
 
             if (isFirstTime){
-                val editor: SharedPreferences.Editor = welcomeScreen.edit()
+                val editor: SharedPreferences.Editor = splashScreen.edit()
                 editor.putBoolean("firstTime", false)
                 editor.commit()
                 findNavController().navigate(R.id.action_spalshFragment_to_welcomeFragment)
             } else {
                 findNavController().navigate(R.id.action_spalshFragment_to_welcomeFragment)
             }
-        }
+        }, 5000)
     }
 }
